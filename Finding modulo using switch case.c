048b4f0e96b943d346a9b5b1b834db53dba7b5b3
Akash99-eng <80410694+Akash99-eng@users.noreply.github.com>
2021-03-27 21:46:16 +0530

    Create Finding modulo using switch case.c

---
+++ Finding modulo using switch case.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+int a=30,b=20;
+int modulo;
+Modulo=a%b;
+    int num = 1;
+
+    switch (num) {
+        case 1:
+        printf("%d", modulo);
+        break;
+        }
+        }
